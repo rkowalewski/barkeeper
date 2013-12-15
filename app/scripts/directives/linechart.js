4afcd0611b1ffb03c938662a6bfc184ed09569af
rkowalewski <roger.kowalewski@gmail.com>
2013-12-15 21:19:02 +0100

    added underscore and refactored the line chart

--- app/scripts/directives/linechart.js
+++ app/scripts/directives/linechart.js
@@ -8,73 +8,92 @@ angular.module('barkeeper.lineChart', ['d3'])
             },
             link: function (scope, element, attrs) {
 
+                var margin = {top: 20, right: 20, bottom: 30, left: 50},
+                    width = 960 - margin.left - margin.right,
+                    height = 500 - margin.top - margin.bottom;
+
                 d3Service.d3().then(function (d3) {
+                    var parseDate = d3.time.format('%d-%b-%y').parse;
 
-                    var margin = {top: 20, right: 20, bottom: 30, left: 50},
-                        width = 960 - margin.left - margin.right,
-                        height = 500 - margin.top - margin.bottom;
-
-                    var parseDate = d3.time.format("%d-%b-%y").parse;
-
-                    var x = d3.time.scale()
-                        .range([0, width]);
-
-                    var y = d3.scale.linear()
-                        .range([height, 0]);
-
-                    var xAxis = d3.svg.axis()
-                        .scale(x)
-                        .orient("bottom");
-
-                    var yAxis = d3.svg.axis()
-                        .scale(y)
-                        .orient("left");
-
-                    var line = d3.svg.line()
-                        .x(function (d) {
-                            return x(d.date);
-                        })
-                        .y(function (d) {
-                            return y(d.close);
-                        });
-
-                    var svg = d3.select("body").append("svg")
+                    var svg = d3.select(element[0]).append('svg')
                         .attr("width", width + margin.left + margin.right)
-                        .attr("height", height + margin.top + margin.bottom)
-                        .append("g")
-                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                        .attr('height', height + margin.top + margin.bottom);
 
-                    scope.data.forEach(function (d) {
-                        d.date = parseDate(d.date);
-                        d.close = +d.close;
+                    // Browser onresize event
+                    window.onresize = function () {
+                        scope.$apply();
+                    };
+
+                    // Watch for resize event
+                    scope.$watch(function () {
+                        return angular.element($window)[0].innerWidth;
+                    }, function () {
+                        scope.render(scope.data);
                     });
 
-                    x.domain(d3.extent(scope.data, function (d) {
-                        return d.date;
-                    }));
-                    y.domain(d3.extent(scope.data, function (d) {
-                        return d.close;
-                    }));
 
-                    svg.append("g")
-                        .attr("class", "x axis")
-                        .attr("transform", "translate(0," + height + ")")
-                        .call(xAxis);
+                    scope.render = function (data) {
+                        svg.selectAll('*').remove();
 
-                    svg.append("g")
-                        .attr("class", "y axis")
-                        .call(yAxis)
-                        .append("text")
-                        .attr("transform", "rotate(-90)")
-                        .attr("y", 6)
-                        .attr("dy", ".71em")
-                        .style("text-anchor", "end")
-                        .text("Price ($)");
+                        var x = d3.time.scale()
+                            .range([0, width]);
 
-                    svg.append("path")
-                        .datum(scope.data)
-                        .attr("class", "line")
-                        .attr("d", line);
+                        var y = d3.scale.linear()
+                            .range([height, 0]);
+
+                        var xAxis = d3.svg.axis()
+                            .scale(x)
+                            .orient('bottom');
+
+                        var yAxis = d3.svg.axis()
+                            .scale(y)
+                            .orient('left');
+
+                        var line = d3.svg.line()
+                            .x(function (d) {
+                                return x(d.date);
+                            })
+                            .y(function (d) {
+                                return y(d.close);
+                            });
+
+                        var g = svg.append('g')
+                            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+                        if (!_.isDate(data[0].date)) {
+                            data.forEach(function (d) {
+                                d.date = parseDate(d.date);
+                                d.close = +d.close;
+                            });
+                        }
+
+                        x.domain(d3.extent(data, function (d) {
+                            return d.date;
+                        }));
+                        y.domain(d3.extent(data, function (d) {
+                            return d.close;
+                        }));
+
+                        g.append('g')
+                            .attr('class', 'x axis')
+                            .attr('transform', 'translate(0,' + height + ')')
+                            .call(xAxis);
+
+                        g.append('g')
+                            .attr('class', 'y axis')
+                            .call(yAxis)
+                            .append('text')
+                            .attr('transform', 'rotate(-90)')
+                            .attr('y', 6)
+                            .attr('dy', '.71em')
+                            .style('text-anchor', 'end')
+                            .text('Gesamtkosten (€)');
+
+                        g.append('path')
+                            .datum(data)
+                            .attr('class', 'line')
+                            .attr('d', line);
+                    };
                 });
             }
         }
